c58e7312cb80fcbf61a0847c5cd4849902520ecc
Mikaël PIRIO <mikael@pirio.org>
2019-10-09 22:09:40 +0200

    Add brace-style tests

---
+++ tests/braceStyle/tests.js
@@ -0,0 +1,36 @@
+if (a) { b = 2; } else { b = 3; }
+
+if (a) {
+    b = 2;
+} else {
+    b = 3;
+}
+
+if (a) {
+    b = 2;
+}
+else {
+    b = 3;
+}
+
+do {
+    b++;
+} while (a)
+
+try {
+    a = 1;
+} catch (error) {
+    console.error(error);
+} finally {
+    b = 2;
+}
+
+try {
+    a = 1;
+}
+catch(error) {
+    console.error(error);
+}
+finally {
+    b = 2;
+}
